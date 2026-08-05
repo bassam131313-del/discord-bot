28268242862c1f7c3399f7e566070942f8fb24cf
bassam131313-del <bassam_13_13_13@icloud.com>
2026-08-05 10:23:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -25,10 +25,36 @@ const client = new Client({
 const TOKEN = process.env.TOKEN;
 
 
-const prefix = "-";
+const VERIFY_CHANNEL_ID = "1534306655691608135";
+const VERIFY_REQUESTS_CHANNEL_ID = "1534306714604667040";
+const VERIFY_LOG_CHANNEL_ID = "1534306771382960288";
 
-client.once("ready", () => {
+// بنضيف ID رتبة Verified لاحقًا
+const VERIFIED_ROLE_ID = "";
+
+client.once("ready", async () => {
   console.log(`${client.user.tag} is online!`);
+
+  const channel = await client.channels.fetch(VERIFY_CHANNEL_ID).catch(() => null);
+
+  if (!channel) return;
+
+  const embed = new EmbedBuilder()
+    .setTitle("📋 التفعيل")
+    .setDescription("اضغط الزر بالأسفل لبدء التفعيل.")
+    .setColor("Blue");
+
+  const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder()
+      .setCustomId("start_verify")
+      .setLabel("ابدأ التفعيل")
+      .setStyle(ButtonStyle.Primary)
+  );
+
+  await channel.send({
+    embeds: [embed],
+    components: [row],
+  });
 });
 
 client.on("messageCreate", async (message) => {
@@ -73,5 +99,52 @@ client.on("messageCreate", async (message) => {
     message.channel.send("🔓 تم فتح الروم.");
   }
 });
+client.on(Events.InteractionCreate, async (interaction) => {
+  if (!interaction.isButton()) return;
 
-client.login(TOKEN);
+  if (interaction.customId === "start_verify") {
+    const modal = new ModalBuilder()
+      .setCustomId("verify_modal")
+      .setTitle("نموذج التفعيل");
+
+    const name = new TextInputBuilder()
+      .setCustomId("name")
+      .setLabel("اسمك")
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+
+    const age = new TextInputBuilder()
+      .setCustomId("age")
+      .setLabel("عمرك")
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+
+    const roblox = new TextInputBuilder()
+      .setCustomId("roblox")
+      .setLabel("يوزرك روبلوكس")
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+
+    const country = new TextInputBuilder()
+      .setCustomId("country")
+      .setLabel("من وين؟")
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+
+    const image = new TextInputBuilder()
+      .setCustomId("image")
+      .setLabel("رابط صورة حسابك روبلوكس")
+      .setStyle(TextInputStyle.Short)
+      .setRequired(true);
+
+    modal.addComponents(
+      new ActionRowBuilder().addComponents(name),
+      new ActionRowBuilder().addComponents(age),
+      new ActionRowBuilder().addComponents(roblox),
+      new ActionRowBuilder().addComponents(country),
+      new ActionRowBuilder().addComponents(image)
+    );
+
+    await interaction.showModal(modal);
+  }
+});
